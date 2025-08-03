637c0d69c2de0ff99aa7c79b14c5f4ad78dc0a75
l <eh.chahkiev@smartics.ru>
2025-08-03 22:40:20 +0300

    feat: create room form

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,5 +5,9 @@ export const routes: Routes = [
   {
     path: '',
     component: Hub
+  },
+  {
+    path: 'rooms/create',
+    loadComponent: () => import('./create-room/create-room').then(m => m.CreateRoom)
   }
 ]
---
+++ frontend/src/app/create-room/create-room.ts
@@ -0,0 +1,25 @@
+import { Component, ChangeDetectionStrategy, model, computed } from '@angular/core'
+import {MatButtonModule} from '@angular/material/button'
+import {MatInputModule} from '@angular/material/input'
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatChipsModule} from '@angular/material/chips';
+import {MatSelectModule} from '@angular/material/select'
+import { uid } from 'radashi'
+
+@Component({
+  selector: 'app-create-room',
+  imports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatChipsModule, MatSelectModule],
+  templateUrl: './create-room.html',
+  styleUrl: './create-room.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CreateRoom {
+  protected readonly uid = uid(8)
+
+  userName = model('')
+  roomName = model('')
+
+  disabled = computed(() => {
+    return this.userName().length < 1 || this.roomName().length < 1
+  })
+}
