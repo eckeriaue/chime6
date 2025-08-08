36e17dd045c2abb236acd188ad32408e96e8d83e
l <eh.chahkiev@smartics.ru>
2025-08-08 15:52:29 +0300

    feat: create endpoint

--- backend/main.go
+++ backend/main.go
@@ -5,15 +5,34 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/gofiber/fiber/v2"
 	"github.com/redis/go-redis/v9"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"os"
 )
+
+// var ctx = context.Background()
 
 func main() {
 	loadEnv()
 	app := fiber.New()
+	// redis := getRedis()
 
 	app.Get("/", func (c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
+	})
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "*",
+	}))
+	api := app.Group("/api/v1")
+
+	roomsApi := api.Group("/rooms")
+	roomsApi.Get("/create", func(c *fiber.Ctx) error {
+		// redis.Set(ctx, "key", "value", 0)
+		log.Println(string(c.Body()))
+		return c.JSON(map[string]any {
+			"message": "Room created successfully",
+			"room_id": "1234567890",
+		})
 	})
 
 	log.Fatal(app.Listen(":" + os.Getenv("BACKEND_PORT")))
