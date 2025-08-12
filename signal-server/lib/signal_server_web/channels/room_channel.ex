9592023ff8d4c56788daf5cd0751da9a5eac2309
l <eh.chahkiev@smartics.ru>
2025-08-12 23:10:08 +0300

    feat: done signaling

--- signal-server/lib/signal_server_web/channels/room_channel.ex
+++ signal-server/lib/signal_server_web/channels/room_channel.ex
@@ -1,26 +1,16 @@
-defmodule WebrtcServerWeb.RoomChannel do
+# lib/signal_server_web/channels/room_channel.ex
+defmodule SignalServerWeb.RoomChannel do
   use Phoenix.Channel
 
-  # Клиенты присоединяются к комнатам по ID
   def join("room:" <> room_id, _params, socket) do
     {:ok, assign(socket, :room_id, room_id)}
   end
 
-  # Обработка SDP Offer от инициатора
-  def handle_in(%{"offer" => %{"sdp" => sdp}}, socket) do
-    broadcast!(socket, "offer", %{sdp: sdp})
-    {:noreply, socket}
+  def handle_in("offer", %{"sdp" => sdp, "to" => _to}, socket) do
+    broadcast!(socket, "offer", %{"sdp" => sdp, "from" => socket.assigns.room_id})
+    {:reply, :ok, socket}
   end
 
-  # Обработка SDP Answer от ответчика
-  def handle_in(%{"answer" => %{"sdp" => sdp}}, socket) do
-    broadcast!(socket, "answer", %{sdp: sdp})
-    {:noreply, socket}
-  end
-
-  # Обработка ICE-кандидатов
-  def handle_in(%{"candidate" => %{"candidate" => candidate}}, socket) do
-    broadcast!(socket, "candidate", %{candidate: candidate})
-    {:noreply, socket}
-  end
+  def handle_in("answer", payload, socket), do: broadcast!(socket, "answer", payload)
+  def handle_in("ice_candidate", payload, socket), do: broadcast!(socket, "ice_candidate", payload)
 end
--- signal-server/lib/signal_server_web/channels/user_socket.ex
+++ signal-server/lib/signal_server_web/channels/user_socket.ex
@@ -1,8 +1,14 @@
 defmodule SignalServerWeb.UserSocket do
   use Phoenix.Socket
 
+  channel "room:*", SignalServerWeb.RoomChannel
 
-  channel "room:*", YourAppWeb.WebRTCCannel
+  def connect(params, socket) do
+    case params["vsn"] do
+      "2.0.0" -> {:ok, socket}
+      _ -> :error
+    end
+  end
 
   def id(_socket), do: nil
 end
